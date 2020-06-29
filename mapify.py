b78cd21dd5ba1d65cc730c31930c113fc8c12e35
borb <perttu.tuovinen@buutti.com>
2020-06-29 16:43:37 +0300

    added room numbering!

--- mapify.py
+++ mapify.py
@@ -18,6 +18,22 @@ TILE_SIZE = 8
 ROOM_WIDTH = 240
 ROOM_HEIGHT = 136
 
+HIDDEN_ROOMS = [
+	11, 12, 13, 14, 15, 16, 17, 18,
+	21, 22, 			26, 27, 28,
+	31, 32, 				37, 38,
+	41, 42, 					48,
+	51, 52, 53, 54, 		57, 58,
+	61, 62, 63, 64, 65, 66, 67, 68,
+	71, 72, 73, 74, 75, 76, 77, 78,
+	81, 82, 83, 84, 85, 86, 87, 88
+]
+REMOVE_COLUMNS = [1, 2, 8]
+REMOVE_ROWS = [1, 6, 7, 8]
+
+def room_number(x, y):
+	return y * 10 + x
+
 def split_to_chunks(text, chunk_size):
 	return textwrap.wrap(text, chunk_size)
 
@@ -122,27 +138,31 @@ def save_tile_as_png(cart, tile_id, modify_palette = None):
 		rows.append(row)
 	png.from_array(rows, "RGB").save("tile_{}.png".format(tile_id))
 
-def room_number(x, y):
-	return y * 10 + x
+def number_array(n, numbers_array): #n=number to print
+	number_width = 6 #pixels
+	channels = 4
+	return [row[(n * number_width) * channels : ((n + 1) * number_width) * channels] for row in numbers_array]
 
-HIDDEN_ROOMS = [
-	11, 12, 13, 14, 15, 16, 17, 18,
-	21, 22, 			26, 27, 28,
-	31, 32, 				37, 38,
-	41, 42, 					48,
-	51, 52, 53, 54, 		57, 58,
-	61, 62, 63, 64, 65, 66, 67, 68,
-	71, 72, 73, 74, 75, 76, 77, 78,
-	81, 82, 83, 84, 85, 86, 87, 88
-]
-REMOVE_COLUMNS = [1, 2, 8]
-REMOVE_LINES = [1, 6, 7, 8]
+def draw_number(n, x, y, image, numbers_array):
+	number = number_array(n, numbers_array)
+	for row_i in range(len(number)):
+		row = number[row_i]
+		del row[3::4] #remove alpha channel
+		for r in range(0, len(row), 3):
+			g = r + 1
+			b = r + 2
+			if row[r]==208 and row[g]==70 and row[b]==72: #only draw red
+				image[row_i + y][r + x * 3] = row[r]
+				image[row_i + y][g + x * 3] = row[g]
+				image[row_i + y][b + x * 3] = row[b]
+	return image
 
-def save_map_as_png(cart, cartname, modify_palette = None, borders = False, hidden = False):
+def save_map_as_png(cart, cartname, modify_palette = None, borders = False, hidden = False, numbers = False):
 	tiles = get_tiles(cart)
 	palette = get_palette(cart, modify_palette)
 	map_cells = get_map(cart)
 	image = []
+
 	# loop over the pixels of the image and get the corresponding color
 	for image_y in range(IMAGE_HEIGHT):
 		image_row = []
@@ -160,13 +180,39 @@ def save_map_as_png(cart, cartname, modify_palette = None, borders = False, hidd
 					color = 0
 				else:
 					color = tiles[tile_id][tile_y * TILE_SIZE + tile_x]
+		
+			image_row += palette[color]
+		image.append(image_row)
 
-			if not(hidden and room_x in REMOVE_COLUMNS):
-				image_row += palette[color]
+	#add room numbers
+	if numbers:
+		numbers_array = list(png.Reader("numbers.png").asDirect()[2])
+		for x in range(0, 8):
+			for y in range(0, 8):
+				image = draw_number(x + 1, x * ROOM_WIDTH + 6, y * ROOM_HEIGHT, image, numbers_array)
+				image = draw_number(y + 1, x * ROOM_WIDTH, 		y * ROOM_HEIGHT, image, numbers_array)
 
-		if not(hidden and room_y in REMOVE_LINES):
-			image.append(image_row)
+	if hidden:
+		deleterows=[]
+		deletecolumns=[]
+		for image_x in range(IMAGE_WIDTH * 3):
+			room_x = floor(floor(image_x / 3) / ROOM_WIDTH) + 1
+			if room_x in REMOVE_COLUMNS:
+				deletecolumns.append(image_x)
+		for image_y in range(IMAGE_HEIGHT):
+			image_row = image[image_y]
+			room_y = floor(image_y / ROOM_HEIGHT) + 1
+			if room_y in REMOVE_ROWS:
+				deleterows.append(image_y)
+		for image_y in range(IMAGE_HEIGHT):
+			for image_x in reversed(deletecolumns):
+				del image[image_y][image_x]
+		for image_y in reversed(deleterows):
+			del image[image_y]
+
 	filename = cartname.rstrip(".lua")
+	if numbers:
+		filename += "_numbers"
 	if borders:
 		filename += "_borders"
 	if hidden:
@@ -187,6 +233,7 @@ if __name__ == "__main__":
 	parser.add_argument("--version", action="version", version="%(prog)s {}".format(VERSION))
 	parser.add_argument("--borders", action="store_true", help="draw border lines between rooms")
 	parser.add_argument("--hidden", action="store_true", help="hide rooms listed in HIDDEN_ROOMS array")
+	parser.add_argument("--numbers", action="store_true", help="show room numbers")
 	parser.add_argument("cartfile", type=argparse.FileType("r"))
 	arguments = parser.parse_args()
 	with arguments.cartfile as cartfile:
@@ -197,4 +244,5 @@ if __name__ == "__main__":
 		arguments.cartfile.name,
 		swap_transparent_to_black,
 		arguments.borders,
-		arguments.hidden )
+		arguments.hidden,
+		arguments.numbers )
